6ba2d3f512038434191d2e61ce136bbbb7fee646
scoot-skoot <o.david.ajao@gmail.com>
2025-10-18 15:47:54 -0500

    Added first API integration: Weather, returns conditions of city and temperature, also helper method in weather as dict to be fed to llm)

--- routes/weather.py
+++ routes/weather.py
@@ -1,6 +1,42 @@
 from flask import Blueprint, jsonify
+import os, requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# This function is for the LLM (returns as dict)
+def fetch_weather_data(city): 
+    # Makes the requests call and returns the dict
+    data = requests.get(...).json()
+    return {
+        "city": data.get("name"),
+        "temperature": data["main"].get("temp"),
+        "condition": data["weather"][0].get("description")
+    }
+
 weather_bp = Blueprint("weather", __name__)
 
+#This funtion is for the frontend (returns as response object)
 @weather_bp.route("/<city>")
 def get_weather(city):
-    return jsonify({"city": city, "temp": 25, "condition": "Sunny"})
+    api_key = os.getenv("WEATHER_API_KEY")
+    if not api_key:
+        return jsonify({"error": "API key not found"}), 500
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial"
+    try:
+        res = requests.get(url)
+        res.raise_for_status()  #Raises an error if status code is not 200, (Went wrong)
+        data = res.json()
+       #Send data back to frontend
+
+        output = {
+            "city": data.get("name"),
+            "temperature": data["main"].get("temp"),
+            "condition": data["weather"][0].get("description")
+        }
+        return jsonify(output)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+    
